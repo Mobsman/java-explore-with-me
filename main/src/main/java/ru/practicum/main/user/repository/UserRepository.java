6af833f5523cd396cee1572baf5a372a50cea802
Azat Bay <mobsman@yandex.ru>
2022-10-13 16:03:21 +0300

    commit

--- main/src/main/java/ru/practicum/main/user/repository/UserRepository.java
+++ main/src/main/java/ru/practicum/main/user/repository/UserRepository.java
@@ -3,6 +3,7 @@ package ru.practicum.main.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.main.user.entity.User;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
